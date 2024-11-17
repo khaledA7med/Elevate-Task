779db14cd8171ccdf0bbe67d8a6b5a8edae08335
Khalid Ahmed Ahmed <khalid.ahmed@oasisoft.net>
2024-11-17 16:02:44 +0200

    []

--- src/app/shared/models/routes/appRoutes.ts
+++ src/app/shared/models/routes/appRoutes.ts
@@ -1,4 +1,7 @@
 export const AppRoutes = {
   Home: '',
   products: 'products',
+  categories: 'products/categories',
+  productsByCategory: 'products/category/',
+  getProductDetails: 'products/',
 };
--- src/app/shared/services/products.service.ts
+++ src/app/shared/services/products.service.ts
@@ -1,28 +1,32 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+import { AppRoutes } from '../models/routes/appRoutes';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductsService {
+  private readonly env: string = environment.baseURL;
+
   constructor(private http: HttpClient) {}
 
   getAllCategories(): Observable<any> {
-    return this.http.get<any>('https://fakestoreapi.com/products/categories');
+    return this.http.get<any>(this.env + AppRoutes.categories);
   }
 
   getAllProducts(): Observable<any> {
-    return this.http.get<any>('https://fakestoreapi.com/products');
+    return this.http.get<any>(this.env + AppRoutes.products);
   }
 
   getProductsByCategory(category: string): Observable<any> {
     return this.http.get<any>(
-      `https://fakestoreapi.com/products/category/${category}`
+      this.env + AppRoutes.productsByCategory + `${category}`
     );
   }
 
   getProductDetails(id: number): Observable<any> {
-    return this.http.get(`https://fakestoreapi.com/products/${id}`);
+    return this.http.get(this.env + AppRoutes.getProductDetails + `${id}`);
   }
 }
